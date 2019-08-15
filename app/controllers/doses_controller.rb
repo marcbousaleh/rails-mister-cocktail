222de7f0187518de29e9b784dcbcd51e64dccfd3
Marc BouSaleh <marcabisaleh@gmail.com>
2019-08-15 16:00:43 +0100

    html done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktails', to: 'cocktails#index'
+
+  # get 'cocktails/new', to: 'cocktails#new'
+  # post 'cocktails', to: 'cocktails#create'
+
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+
+  # get 'cocktails/:id/doses/new', to: 'doses#new', as: :cocktail_doses
+  # post 'cocktails/:id/doses', to: 'doses#create'
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
